9a032ecfb0b803cb5d67b957e153a9700c80d96b
Ronke Adesoye <80988672+f1y1n@users.noreply.github.com>
2025-02-05 21:09:21 +0100

    Add files via upload

---
+++ Folder/Team glycine stage zero.py
@@ -0,0 +1,54 @@
+
+# Team Glycine
+Team_glycine = [
+    {
+    "Name": "Adesoye Ronke",
+    "Slack_name": "Ronke",
+    "Email": "fiyinfoluwaaderonke99@gmail.com",
+    "Hobby": "Football",
+    "Country": "Nigeria",
+    "Discipline": "Microbiology",
+    "Prog_language": "python"
+},
+{
+    "Name": "Ghizal",
+    "Slack_name": "Ghizal",
+    "Email": "Ghizal29@gmail.com",
+    "Hobby": "Reading",
+    "Country": "Germany",
+    "Discipline": "pharma",
+    "Prog_Language": "Python"
+},
+{
+    "Name": "Adekunle Oluwafikayomi Blessing",
+    "Slack_name": "Oluwafikayomi",
+    "Email": "oluwafikayomiblessinga@gmail.com",
+    "Hobby": "Reading",
+    "Country": "Nigeria",
+    "Discipline": "Biochemistry",
+    "Prog_Language": "Python"
+},
+]
+print(f"Name: {Team_glycine[0]['Name']}")
+print(f"Slack_name: {Team_glycine[0]['Slack_name']}")
+print(f"Email: {Team_glycine[0]['Email']}")
+print(f"Hobby: {Team_glycine[0]['Hobby']}")
+print(f"Country: {Team_glycine[0]['Country']}")
+print(f"Discipline: {Team_glycine[0]['Discipline']}")
+print(f"Prog_language: {Team_glycine[0]['Prog_language']}")
+print("")
+print(f"Name: {Team_glycine[1]['Name']}")
+print(f"Slack_name: {Team_glycine[1]['Slack_name']}")
+print(f"Email: {Team_glycine[1]['Email']}")
+print(f"Hobby: {Team_glycine[1]['Hobby']}")
+print(f"Country: {Team_glycine[1]['Country']}")
+print(f"Discipline: {Team_glycine[1]['Discipline']}")
+print(f"Prog_language: {Team_glycine[0]['Prog_language']}")
+print("")
+print(f"Name: {Team_glycine[2]['Name']}")
+print(f"Slack_name: {Team_glycine[2]['Slack_name']}")
+print(f"Email: {Team_glycine[2]['Email']}")
+print(f"Hobby: {Team_glycine[2]['Hobby']}")
+print(f"Country: {Team_glycine[2]['Country']}")
+print(f"Discipline: {Team_glycine[2]['Discipline']}")
+print(f"Prog_language: {Team_glycine[2]['Prog_language']}")
